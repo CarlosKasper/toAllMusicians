c0034a4a02b859769cd28a3986076dff7bb6c0ca
Carlos Eduardo Kasper <carlos.kasper@cwi.com.br>
2021-12-07 19:48:28 -0300

    fix request axios

--- app/src/hooks/api/api.hook.js
+++ app/src/hooks/api/api.hook.js
@@ -7,7 +7,7 @@ export function useApi() {
 	const token = user;
 	const apiUrl = window.location.href.includes('localhost')
 		? 'http://localhost:8090'
-		: 'toallmusicians.brazilsouth.azurecontainer.io';
+		: 'http://toallmusicians.brazilsouth.azurecontainer.io';
 	const axios = useAxios(apiUrl, {
 		Authorization: 'Bearer ' + token,
 	});
