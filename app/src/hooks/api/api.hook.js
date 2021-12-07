765306881fd36f444a6fcffc9497217ad9cd2ba6
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-12-07 00:57:31 -0300

    fix api url

--- app/src/hooks/api/api.hook.js
+++ app/src/hooks/api/api.hook.js
@@ -7,7 +7,7 @@ export function useApi() {
 	const token = user;
 	const apiUrl = window.location.href.includes('localhost')
 		? 'http://localhost:8090'
-		: 'https://toallmusicians.brazilsouth.azurecontainer.io';
+		: 'toallmusicians.brazilsouth.azurecontainer.io';
 	const axios = useAxios(apiUrl, {
 		Authorization: 'Bearer ' + token,
 	});
